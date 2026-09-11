81ba16185ba2f459d64e9243906a7345c3bae8bd
Ballaam <theballaam96@gmail.com>
2026-09-11 08:43:44 -0500

    First Commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+echo "Creating ./bin directory..."
+mkdir -p ./bin
+
+echo "Running RecompModTool..."
+./RecompModTool ./mod.toml ./bin
+
+echo "Zipping output file into ./bin..."
+zip -j ./bin/translation_german.zip ./bin/translation_german.nrm
+
+echo "Complete"
